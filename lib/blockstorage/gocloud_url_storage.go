2189787807d45fec151ab2c6c5c524a99da18102
Ulrich Hornung <hornunguli@gmx.de>
2024-12-03 19:24:48 +0100

    test 10 channels

--- lib/blockstorage/gocloud_url_storage.go
+++ lib/blockstorage/gocloud_url_storage.go
@@ -397,7 +397,7 @@ func IsDone(ctx context.Context) bool {
 
 func (hm *GoCloudUrlStorage) IterateBlocks(ctx context.Context, fn func(d HashAndState)) error {
 
-	numberOfParallelChannels := 4
+	numberOfParallelChannels := 10
 	chanOfChannels := make(chan chan HashStateAndError, numberOfParallelChannels-1)
 
 	connections := make([]*GoCloudUrlStorage, 0, numberOfParallelChannels)
